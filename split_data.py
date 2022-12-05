2579a1e1d642d260f7710cebc0bb9ed7f91d1a11
murdi@sm <murdi@sm.com>
2022-12-05 18:43:42 +0400

    split data

---
+++ split_data.py
@@ -0,0 +1,5 @@
+'''
+The original dataset contains 11,000 images of 11 classes,each of which contains 1,000 examples.
+ We randomly se-lect 200 examples from each class respectively to constructour validation set with total 2,200 images. 
+The rest of origi-nal dataset is our training set,i.e., the training-validation ratiois 4:1.
+'''
