1db39563d11193dfa98e6a5ebbad06ad4627cc5e
murdi@sm <murdi@sm.com>
2022-12-06 23:46:53 +0400

    main

--- main.py
+++ main.py
@@ -14,6 +14,41 @@ params = {'batch_size': 64,
           'num_workers': 6}
 max_epochs = 100
 
+# class LoadDataset(torch.utils.data.Dataset):
+#     def __init__(self, data):
+#         super(LoadDataset, self).__init__()
+#         # List of files
+#         self.data_files = # [DATA_FOLDER.format(id) for id in ids]
+#         self.label_files = [LABELS_FOLDER.format(id) for id in ids]
+#         # Sanity check : raise an error if some files do not exist
+#         for f in self.data_files + self.label_files:
+#             if not os.path.isfile(f):
+#                 raise KeyError("{} is not a file !".format(f))
+
+#     def __len__(self):
+#         return len(self.data_files)  # the length of the used data
+
+#     def __getitem__(self, idx):
+#         #         Pre-processing steps
+#         #     # Data is normalized in [0, 1]
+#         self.data = (
+#                 1
+#                 / 255
+#                 * np.asarray(
+#             io.imread(self.data_files[idx], plugin="pil").transpose((2, 0, 1)),
+#             dtype="float32",
+#         )
+#         )
+#         self.label = np.asarray(
+#             convert_from_color(io.imread(self.label_files[idx], plugin="pil")),
+#             dtype="int64",
+#         )
+#         # print("label = ", self.label.shape)
+#         data_p, label_p = self.data, self.label
+#         # Return the torch.Tensor values
+#         return torch.from_numpy(data_p), torch.from_numpy(label_p)
+
+
 
 def split_dataset(train,target,validation=False):
     #70%-20%-10% split, as we're splitting 10% from the already split X_train so we're actually ending up with a 72%-20%-8% split here:
@@ -25,52 +60,52 @@ def split_dataset(train,target,validation=False):
 
     if validation:
         X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, train_size=0.9)
-        train_data = [np.squeeze(X_train, axis=1),y_train]
-        validation_data = [np.squeeze(X_train, axis=1),y_valid]
+        train_data = [np.squeeze(X_train, axis=1),y_train.to_numpy()]
+        validation_data = [np.squeeze(X_train, axis=1),y_valid.to_numpy()]
 
         return train_data, validation_data, test_data
 
-    train_data = [np.squeeze(X_train, axis=1),y_train]
-    test_data = [np.squeeze(X_train, axis=1),y_test]
+    train_data = [np.squeeze(X_train, axis=1),y_train.to_numpy()]
+    test_data = [np.squeeze(X_train, axis=1),y_test.to_numpy()]
 
     return train_data,test_data
 
 
-def train(data_loader, model, optimizer,epochs=100):
-    ''' 
-        sigma:  (num_samples,num_mixtures,2,2) 
-        pi:     (num_samples,num_mixtures)
-        mue:    (num_samples,num_mixtures,2)
+# def train(data_loader, model, optimizer,epochs=100):
+#     ''' 
+#         sigma:  (num_samples,num_mixtures,2,2) 
+#         pi:     (num_samples,num_mixtures)
+#         mue:    (num_samples,num_mixtures,2)
 
-        The last parameter '2' represents x and y  
-    '''
-    num_batches = len(data_loader)
-    total_loss = 0
-    model.train()
+#         The last parameter '2' represents x and y  
+#     '''
+#     num_batches = len(data_loader)
+#     total_loss = 0
+#     model.train()
     
-    for i in range (epochs):
-        total_loss = 0
-        for X, y in data_loader:
-            #print(X.shape)
-            x_variable = Variable(X,requires_grad=True).to(device)
-            X = X.to(device)
-            #y = y.to(device)
-            pi, sigma_x,sigma_y, mu_x , mu_y = model(x_variable)
-            #pi_variable, sigma_variable, mu_variable = model(x_variable)
-            #print(f"sigma_variable{sigma_variable.shape}")
-            loss = mdn_loss_fn( pi, sigma_x,sigma_y, mu_x , mu_y, y)
-            # loss = Variable(loss, requires_grad = True)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+#     for i in range (epochs):
+#         total_loss = 0
+#         for X, y in data_loader:
+#             #print(X.shape)
+#             x_variable = Variable(X,requires_grad=True).to(device)
+#             X = X.to(device)
+#             #y = y.to(device)
+#             pi, sigma_x,sigma_y, mu_x , mu_y = model(x_variable)
+#             #pi_variable, sigma_variable, mu_variable = model(x_variable)
+#             #print(f"sigma_variable{sigma_variable.shape}")
+#             loss = mdn_loss_fn( pi, sigma_x,sigma_y, mu_x , mu_y, y)
+#             # loss = Variable(loss, requires_grad = True)
+#             optimizer.zero_grad()
+#             loss.backward()
+#             optimizer.step()
 
-            total_loss += loss.item()
+#             total_loss += loss.item()
 
-        avg_loss = total_loss / num_batches
-        if (i+1)%5 == 0:
-            print(f"Epoch {i+1} train loss: {avg_loss}")
-    print(f"Epoch {i+1} train loss: {avg_loss}")
-    
+#         avg_loss = total_loss / num_batches
+#         if (i+1)%5 == 0:
+#             print(f"Epoch {i+1} train loss: {avg_loss}")
+#     print(f"Epoch {i+1} train loss: {avg_loss}")
+
 if __name__ == '__main__':
     # ['id', 'label', 'xmin', 'ymin', 'xmax', 'ymax', 'img_path']
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -82,8 +117,11 @@ if __name__ == '__main__':
     data = df.to_numpy()
 
     # input and target 
-    input  = df[['img_path']]
-    target = df[['xmin','ymin', 'xmax', 'ymax','label']]
+    input  = df[['img_path']].values
+    input =np.squeeze(input)
+    # input = input.reset_index()
+    target = df[['xmin','ymin', 'xmax', 'ymax','label']].values
+    print(target)
 
     # count by groups around 1k for each class
     # group_df = df.groupby('label',sort=True).count()
@@ -91,9 +129,11 @@ if __name__ == '__main__':
 
     # splitting data
     #train_data, validation_data, test_data = split_dataset(input,target,True)
-    train_data, test_data = split_dataset(input,target,False)
+    # train_data, test_data = split_dataset(input,target,False)
+
+    # print(train_data[0],train_data[1][0].ravel())
     
-    # Dataloaders
-    train_dataloader = torch.utils.data.DataLoader(train_data, params['batch_size'],num_workers=params['num_workers'])
-    #validation_dataloader = torch.utils.data.DataLoader(validation_data, params['batch_size'],num_workers=params['num_workers'])
-    test_dataloader = torch.utils.data.DataLoader(test_data, params['batch_size'],num_workers=params['num_workers'])
+    # # Dataloaders
+    # train_dataloader = torch.utils.data.DataLoader(train_data, params['batch_size'],num_workers=params['num_workers'])
+    # #validation_dataloader = torch.utils.data.DataLoader(validation_data, params['batch_size'],num_workers=params['num_workers'])
+    # test_dataloader = torch.utils.data.DataLoader(test_data, params['batch_size'],num_workers=params['num_workers'])
