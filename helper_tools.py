32cb00561bf4185bfcca94982636c4d75f59bd4c
awetmokie <awetmokie@gmail.com>
2022-12-10 10:53:36 +0400

    helper_tools update

--- helper_tools.py
+++ helper_tools.py
@@ -229,117 +229,6 @@ def get_default_device():
 
 
 
-class CNN(nn.Module):
-    def __init__(self, input_channels=3, filters=32, num_classes=11):
-        super(CNN, self).__init__()
-        self.conv1  = nn.Conv2d(input_channels, filters, kernel_size=2) 
-        self.conv2  = nn.Conv2d(filters, filters * 2, kernel_size=3)
-
-        self.fc1  =  nn.Linear(in_features=  (filters * 2 * 56*56  + 2048) , out_features=2)
-        self.out = nn.Linear(2, out_features=num_classes)
-
-        self.flatten = nn.Flatten(start_dim=1)
-
-    def forward(self, x, x2):
-        x  = self.conv1(x) # output = (B, C,224,224)
-        x  = F.relu(x) 
-        x  = F.max_pool2d(x, kernel_size=2, padding=1) #output = (B,C,112,112)
-        x  = F.dropout(x, 0.2)
-
-        x  = self.conv2(x) #output = (B,iC,112,112)
-        x  = F.relu(x)
-        x  = F.max_pool2d(x, kernel_size=2, padding=1) # #output = (B,iC,56,56)
-        x  = F.dropout(x, 0.2)
-    
-        x  = self.flatten(x)
-        x2  = self.flatten(x2)
-        x  = torch.cat((x, x2), dim=1)
-        x  = self.fc1(x)
-        x  = F.relu(x)
-        x  = F.dropout(x, 0.25)
-        x  = self.out(x)
-        return x
-
-
-class tinyCNN(nn.Module):
-    def __init__(self, input_channels=3, filters=32, num_classes=11):
-        super(tinyCNN, self).__init__()
-        self.conv1  = nn.Conv2d(input_channels, filters, kernel_size=2) 
-        self.conv2  = nn.Conv2d(filters, filters * 2, kernel_size=3)
-        self.out = nn.Linear(filters * 2 * 56*56, out_features=num_classes)
-
-        self.flatten = nn.Flatten(start_dim=1)
-
-    def forward(self, x):
-        x  = self.conv1(x) # output = (B, C,224,224)
-        x  = F.relu(x) 
-        x  = F.max_pool2d(x, kernel_size=2, padding=1) #output = (B,C,112,112)
-        x  = F.dropout(x, 0.2)
-
-        x  = self.conv2(x) #output = (B,iC,112,112)
-        x  = F.relu(x)
-        x  = F.max_pool2d(x, kernel_size=2, padding=1) # #output = (B,iC,56,56)
-        x  = F.dropout(x, 0.2)
-
-        x  = self.flatten(x)
-        x  = self.out(x)
-        return x
-
-
-def trainCustomModel(resnetModel,yoloDetectModel,custom_cnn_classifier, train_dataloader,optimizer,loss_fn, epochs=30, learning_rate=0.001, device='cpu'):
-    """Accepts feature from resnet 
-    and yolo object detection cropped iamge(s) 
-    as features to train an accurate cnn classifier.
-    """
-
-    training_losses = []
-    training_accs = []
-
-    for epoch in range(1, epochs+1):
-        number_of_batches = 0
-        epoch_loss_values = 0.0
-        epoch_accs = 0.0
-        for index, (X1, y) in enumerate(tqdm(train_dataloader)):
-            # put tensors in gpu state
-            X1  = Variable(X1, requires_grad=True).to(device)
-            # X2 = Variable(X2, requires_grad=True).to(device)
-            y  = y.to(device)
-
-            # resnet processing
-            # yolo_X  = yoloDetectModel(X1)
-
-            resnet_X  = resnetModel(X1)
-
-            # extracted_features = resnetModel(resnet_X) # (Bacth, 1000) tensor is returned
-            # X2  = Variable(torch.randn(size= (X.size()[0],1000)), requires_grad=True).to(device)
-            preds = custom_cnn_classifier(X1, resnet_X)
-        
-
-            loss = loss_fn(preds, y).to(device)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            probs = torch.log_softmax(preds, dim=1)
-            predicted_labels = torch.argmax(probs, dim=1)
-
-            # acc
-            epoch_accs += accuracy_score(y.detach().cpu(),predicted_labels.detach().cpu())
-            epoch_loss_values += loss.item()
-
-            number_of_batches += 1
-
-        # compute average of batch loss and accuracy
-        batch_acc, batch_loss = epoch_accs / \
-            number_of_batches, epoch_loss_values / number_of_batches
-        training_losses.append(batch_loss)
-        training_accs.append(batch_acc)
-
-        print("Epoch:{}/{}, acc={:.3f}%, loss={:.3f}".format(epoch,epochs, batch_acc*100, batch_loss))
-
-    print("Learning Finished!")
-
-    return custom_cnn_classifier, training_accs, training_losses
 
 
 class TinyDataset(torch.utils.data.Dataset):
@@ -415,3 +304,60 @@ def get_pretrained_model():
         param.requires_grad = False
 
     return resnet152_model
+
+
+
+def trainCustomModel(resnetModel,yoloDetectModel,custom_cnn_classifier, train_dataloader,optimizer,loss_fn, epochs=30, learning_rate=0.001, device='cpu'):
+    """Accepts feature from resnet 
+    and yolo object detection cropped iamge(s) 
+    as features to train an accurate cnn classifier.
+    """
+
+    training_losses = []
+    training_accs = []
+
+    for epoch in range(1, epochs+1):
+        number_of_batches = 0
+        epoch_loss_values = 0.0
+        epoch_accs = 0.0
+        for index, (X1, y) in enumerate(tqdm(train_dataloader)):
+            # put tensors in gpu state
+            X1  = Variable(X1, requires_grad=True).to(device)
+            # X2 = Variable(X2, requires_grad=True).to(device)
+            y  = y.to(device)
+
+            # resnet processing
+            # yolo_X  = yoloDetectModel(X1)
+
+            resnet_X  = resnetModel(X1)
+
+            # extracted_features = resnetModel(resnet_X) # (Bacth, 1000) tensor is returned
+            # X2  = Variable(torch.randn(size= (X.size()[0],1000)), requires_grad=True).to(device)
+            preds = custom_cnn_classifier(X1, resnet_X)
+        
+
+            loss = loss_fn(preds, y).to(device)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            probs = torch.log_softmax(preds, dim=1)
+            predicted_labels = torch.argmax(probs, dim=1)
+
+            # acc
+            epoch_accs += accuracy_score(y.detach().cpu(),predicted_labels.detach().cpu())
+            epoch_loss_values += loss.item()
+
+            number_of_batches += 1
+
+        # compute average of batch loss and accuracy
+        batch_acc, batch_loss = epoch_accs / \
+            number_of_batches, epoch_loss_values / number_of_batches
+        training_losses.append(batch_loss)
+        training_accs.append(batch_acc)
+
+        print("Epoch:{}/{}, acc={:.3f}%, loss={:.3f}".format(epoch,epochs, batch_acc*100, batch_loss))
+
+    print("Learning Finished!")
+
+    return custom_cnn_classifier, training_accs, training_losses
